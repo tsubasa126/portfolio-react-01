6c05117be0985de83974174b310b2cf595d005d6
Keith Noro <141191549+tsubasa126@users.noreply.github.com>
2024-04-11 20:58:55 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">
+    {<section id="Services">
       <Parallax type="services"/>
-    </section>
+    </section>}
     {/* <section>
       <Services />
     </section> */}
